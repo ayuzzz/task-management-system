0b12673548afdf7dc60e21a541a039f5e960b0d5
AYUSH JOSHI <ayush.joshi@siemens-healthineers.com>
2024-09-16 23:42:43 +0530

    initial backend commit

---
+++ backend/Controllers/TaskController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        public TaskController()
+        {
+            
+        }
+    }
+}
